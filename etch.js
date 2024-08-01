965050871ffda5f9f593ec25bbbfd985e38f7ed5
Maybe a Replicant <dan990ov@gmail.com>
2024-08-01 11:40:29 +0200

    Added mode and erase buttons.

--- etch.js
+++ etch.js
@@ -1,10 +1,18 @@
 // DOM
 const divGrid = document.getElementById("etch-container"),
   divConfig = document.getElementById("etch-config"),
-  btnDraw4 = document.getElementById("grid-draw-4"),
   btnDraw8 = document.getElementById("grid-draw-8"),
   btnDraw16 = document.getElementById("grid-draw-16"),
-  btnDraw32 = document.getElementById("grid-draw-32");
+  btnDraw32 = document.getElementById("grid-draw-32"),
+  btnDraw64 = document.getElementById("grid-draw-64"),
+  btnErase = document.getElementById("grid-clear"),
+  btnPsych = document.getElementById("mode-psych"),
+  btnClassic = document.getElementById("mode-classic");
+
+let gridTileN,
+  gridTileX,
+  gridTileY,
+  drawMode = "psych";
 
 // Color randomizer
 function rg(m, n) {
@@ -28,7 +36,6 @@ function randColor() {
 }
 
 // Grid generator
-let gridTileN, gridTileX, gridTileY;
 
 function drawGrid(gridTileN) {
   // First we delete all already created tiles
@@ -45,17 +52,39 @@ function drawGrid(gridTileN) {
     for (let drawnY = 0; drawnY < gridTileN; drawnY++) {
       gridTileY = document.createElement("div");
       gridTileY.classList.add("tile-y");
-      gridTileY.addEventListener("mouseover", function () {
-        this.style.cssText = `background: ${randColor()};`;
-      });
+      if (drawMode === "psych") {
+        gridTileY.addEventListener("mouseover", function () {
+          this.style.cssText = `background: ${randColor()};`;
+        });
+      } else {
+        gridTileY.addEventListener("mouseover", function () {
+          this.style.cssText = `background: #333`;
+        });
+      }
       gridTileX.appendChild(gridTileY);
     }
   }
 }
 
-btnDraw4.onclick = () => drawGrid(4);
 btnDraw8.onclick = () => drawGrid(8);
 btnDraw16.onclick = () => drawGrid(16);
 btnDraw32.onclick = () => drawGrid(32);
+btnDraw64.onclick = () => drawGrid(64);
+btnErase.onclick = () => {
+  const gridTiles = document.querySelectorAll(".tile-x > div");
 
-drawGrid(4);
+  gridTiles.forEach((item) => {
+    const gridItem = item;
+    gridItem.style.backgroundColor = "#eee";
+    gridItem.style.opacity = "1";
+    gridItem.count = 0;
+  });
+};
+btnClassic.onclick = () => {
+  drawMode = "classic";
+};
+btnPsych.onclick = () => {
+  drawMode = "psych";
+};
+
+drawGrid(8);
